9b49a65a6cd542c791a5c302ba95ebaffa95b7f0
P@ul <MyEmail@Zoho.com>
2022-05-08 17:48:46 +0530

    feat - toaster coponent

--- main.js
+++ main.js
@@ -21,6 +21,8 @@ const getClsBtn = () => {
 
 
 
+const CLASS_NAME_HIDE = 'hide-content'
+const CLASS_NAME_SHOW = 'show'
 
 
 //modal demo
@@ -28,20 +30,74 @@ var btn = document.querySelector('#myBtn')
 var modal = document.querySelector('#modal-demo-container')
 var close = document.querySelector('#closeBtn')
 
-btn.addEventListener('click', () => {
-    modal.classList.add('show')
-    modal.classList.remove('hide-content')
+btn?.addEventListener('click', () => {
+    modal.classList.add(CLASS_NAME_SHOW)
+    modal.classList.remove(CLASS_NAME_HIDE)
 })
-close.addEventListener('click', () => {
+close?.addEventListener('click', () => {
     console.log("Hello");
-    modal.classList.remove('show')
-    modal.classList.add('hide-content')
+    modal.classList.remove(CLASS_NAME_SHOW)
+    modal.classList.add(CLASS_NAME_HIDE)
 })
 
 window.addEventListener('click', (event) => {
     if (event.target === modal) {
-        modal.classList.remove('show')
-        modal.classList.add('hide-content')
+        modal.classList.remove(CLASS_NAME_SHOW)
+        modal.classList.add(CLASS_NAME_HIDE)
     }
 })
-console.log("Hello");
+
+
+/* Live ToastB button */
+var liveToastButton = document.querySelector('#liveToastButton')
+var liveToast = document.querySelector('#liveToast')
+var closeBtn = document.querySelector('#closeBtn')
+
+liveToastButton?.addEventListener('click', () => {
+    liveToast.classList.remove(CLASS_NAME_HIDE)
+
+    setTimeout(() => {
+        liveToast.classList.add(CLASS_NAME_HIDE)
+    }, 5000);
+})
+
+closeBtn.addEventListener("click", () => {
+
+    liveToast.classList.add(CLASS_NAME_HIDE)
+})
+
+
+var topRight = document.querySelector('#topRight')
+var bottomRight = document.querySelector('#bottomRight')
+var bottomLeft = document.querySelector('#bottomLeft')
+
+var toastPlacement = document.querySelector('#toastPlacement')
+
+topRight?.addEventListener('click', () => {
+    removeClassList()
+    toastPlacement.classList.add('top-right')
+    hideToaster()
+
+})
+bottomRight?.addEventListener('click', () => {
+    removeClassList()
+    toastPlacement.classList.add('bottom-right')
+    hideToaster()
+
+})
+bottomLeft?.addEventListener('click', () => {
+    removeClassList()
+    toastPlacement.classList.add('bottom-left')
+    hideToaster()
+})
+
+let removeClassList = () => {
+    toastPlacement.classList.remove('top-right', 'bottom-right', 'bottom-left')
+}
+
+let hideToaster = () => {
+    setTimeout(() => {
+        toastPlacement.classList.remove('top-right', 'bottom-right', 'bottom-left')
+    }, 3000);
+}
+
